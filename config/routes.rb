6e267304cad17bf2f29adc359b1266cb30178193
Jim Suchy <jsuchy@gmail.com>
2011-10-30 17:47:44 -0500

    more fixes for rails 3 upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Cookbook::Application.routes.draw do
-  #match 'recipes' => 'recipes#index', :as => :recipes
   match 'recipes/all' => 'recipes#all', :as => :all_recipes
-  #match 'recipes/new' => 'recipes#new', :as => :new_recipe
   match 'login/index' => 'login#index', :as => :login
+  match 'login/attempt_login' => 'login#attempt_login'
+  match 'logout' => 'login#logout'
+  match 'recipes/search' => 'recipes#search'
 
   resources :recipes
 
   root :to => "recipes#index"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
-  match ':controller(/:action(/:id(.:format)))'
+  #match ':controller(/:action(/:id(.:format)))'
 end
