6d2d47353eb928dcc90afddba75964c1f7b631a4
Jim Suchy <jsuchy@gmail.com>
2012-02-17 08:19:33 -0600

    Development uses postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,8 +20,8 @@ ActiveRecord::Schema.define(:version => 17) do
     t.string   "uom"
     t.string   "ingredient"
     t.string   "instruction"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "recipes", :force => true do |t|
@@ -35,8 +36,8 @@ ActiveRecord::Schema.define(:version => 17) do
     t.string   "image_url"
     t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -44,8 +45,8 @@ ActiveRecord::Schema.define(:version => 17) do
     t.string   "email"
     t.string   "hashed_password"
     t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
